82f5eed41fd828f23a85f4e69f3e4a0450bc3018
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-03-27 18:49:48 +0600

    error done

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Movie.css'
@@ -11,9 +13,9 @@ const Movie = ({ movie, handleButton }) => {
         <div className='movie'>
             {/* <h2>Id: {id}</h2> */}
             <img className='img' src={image} alt="" />
-            <h2>Name: {name} </h2>
+            <h3>Name: {name} </h3>
             <p>Price: ${price}</p>
-            <button onClick={() => handleButton(movie)} className='btn-cart'><p>Add to cart</p></button>
+            <button onClick={() => handleButton(movie)} className='btn-cart'><p className='btn-text'>Add to cart</p> <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon></button>
             <ToastContainer />
 
         </div>
